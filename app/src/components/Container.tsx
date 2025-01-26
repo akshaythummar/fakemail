5004ebd0263b4b216dd196d2131446451f39f4e8
CH563 <wonderful563@qq.com>
2025-01-26 18:34:38 +0800

    add dev

--- app/src/components/Container.tsx
+++ app/src/components/Container.tsx
@@ -9,7 +9,7 @@ export default ({ siteKey }: {siteKey?: string}) => {
     const [status, setStatus] = useState<Status | null>(null)
     if (status === 'solved' || import.meta.env.DEV) return (
         <>
-        <h2 className='text-xl font-bold text-center p-6 text-green-500 border border-dashed border-green-300 mt-4 bg-green-300/10'>Sorry, Server is update..</h2>
+        <h2 className='text-xl font-bold text-center p-6 text-green-500 border border-dashed border-green-300 mt-4 bg-green-300/10'>Sorry, the system is being upgraded...</h2>
         </>
     )
     return <Turnstile
