9ea34d3c934098495e40d85251bb51cb2d4ee02d
CH563 <wonderful563@qq.com>
2025-01-26 18:31:15 +0800

    add dev

--- app/src/components/Container.tsx
+++ app/src/components/Container.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { Turnstile } from '@marsidev/react-turnstile';
-import Lists from '@/components/Lists';
 
 type Status = 'error' | 'expired' | 'solved';
 
@@ -8,7 +7,11 @@ const key = import.meta.env.PUBLIC_TURNSTILE_SITE_KEY || '1x00000000000000000000
 
 export default ({ siteKey }: {siteKey?: string}) => {
     const [status, setStatus] = useState<Status | null>(null)
-    if (status === 'solved' || import.meta.env.DEV) return <Lists />
+    if (status === 'solved' || import.meta.env.DEV) return (
+        <>
+        <h2 className='text-xl font-bold text-center p-6 text-green-500 border border-dashed border-green-300 mt-4 bg-green-300/10'>Sorry, Server is update..</h2>
+        </>
+    )
     return <Turnstile
         className='mx-auto text-center py-6'
         siteKey={siteKey || key}
